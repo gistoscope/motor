da51ec76e33fd8b2c1568d9ebe49869121baa422
gistoscope <93454471+gistoscope@users.noreply.github.com>
2025-10-15 10:53:29 -0400

    Update vitest.config.ts

--- packages/tests/vitest.config.ts
+++ packages/tests/vitest.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vitest/config';
-import { vitestAliases } from '../../scripts/aliases.generated.ts';
+import { defineConfig } from 'vitest/config'
+import { vitestAliases } from '../../scripts/aliases.generated'
 
 export default defineConfig({
-  test: {
-    environment: 'node'
-  },
-  resolve: {
-    alias: vitestAliases
-  }
-});
+  test: { environment: 'node' },
+  resolve: { alias: vitestAliases }
+})
